ff692e4fb6a814e1d2af60e1088c407533b571cf
fabien <fabien.moritz@gmail.com>
2014-01-14 12:01:16 +0100

    initialisation avec le transpileur de webcomponents

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
-module.exports = function (grunt) {
-    var tasks = require('inativ-gruntfile')(grunt);
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            components: {
+                files: ['src/*.html'],
+                tasks: ['wctranspile']
+            }
+        },
+
+        wctranspile: {
+            test: {
+                src: ['src/*.html'],
+                css: 'dist/stylesheets/webcomponents.css',
+                js:  'dist/js/webcomponents.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-wctranspile');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['wctranspile']);
+    grunt.registerTask('default', ['build', 'watch']);
+
 };
